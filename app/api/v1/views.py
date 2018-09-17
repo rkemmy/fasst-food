94ce9f80cf07a938dd770f47300028975d21fba1
rkemmy <rkemmy.rk69@gmail.com>
2018-09-18 02:52:29 +0300

    [#160537190] Create a get request

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,2 +1,9 @@
+from flask_restful import Resource
+from flask import jsonify, request
 
-    
+orders_data = []
+
+class Orders(Resource):
+
+    def get(self):
+        return jsonify({"Orders": orders_data})
