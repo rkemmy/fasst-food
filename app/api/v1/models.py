2f4e9d14bb5afc5691d93d178bbb67cd175db510
rkemmy <rkemmy.rk69@gmail.com>
2018-09-18 02:47:54 +0300

    [160537190] create order model

--- app/api/v1/models.py
+++ app/api/v1/models.py
@@ -1,3 +1,12 @@
 
+class Order:
+    order_id = 1
+    def __init__(self,name,description,id,price,status = "pending"):
+        self.name =  name
+        self.description =  description
+        self.id = Order.order_id
+        self.status = status
+        self.price = price
 
+        order_id = +1
      
