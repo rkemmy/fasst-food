b9afefb040c80ccf7f5d041e61d102579c2bf5c1
rkemmy <rkemmy.rk69@gmail.com>
2018-09-18 03:54:47 +0300

    [#160537190] Create a get request api endpoint

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,9 +1,15 @@
 from flask_restful import Resource
-from flask import jsonify, request
+from flask import request
+
 
 orders_data = []
 
 class Orders(Resource):
 
     def get(self):
-        return jsonify({"Orders": orders_data})
+        if len(orders_data) == 0:
+            return ({"message":"empty list"}, 404)
+        else:
+            return ({"Orders": orders_data})
+
+    
